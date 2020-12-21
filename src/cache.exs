297508bbdca52a3c20835c42237b82d765f880e2
Cristina Silva <crissilva.eng@gmail.com>
2020-12-21 05:56:24 +0000

    bump to 0.0.3-dev:wq

--- src/cache.exs
+++ src/cache.exs
@@ -52,9 +52,40 @@ defmodule Cache do
   end
 end
 
-Cache.start_link()
-Cache.put("google.com", "Welcome to Google...")
-Cache.get("google.com")
-Cache.size()
-Cache.put("amazon.com", nil)
+defmodule CacheSupervisor do
+  def start do
+    spawn(__MODULE__, :loop_system, [])
+  end
+
+  def loop do
+    pid = Cache.start_link()
+
+    receive do
+      {:EXIT, ^pid, :normal} ->
+        IO.puts("Cache exited normally.")
+        :ok
+
+      {:EXIT, ^pid, reason} ->
+        IO.puts("Cache failed with reason #{inspect(reason)} - restarting it.")
+        loop()
+    end
+
+    loop()
+  end
+
+  def loop_system do
+    Process.flag(:trap_exit, true)
+    loop()
+  end
+end
+
+# CacheSupervisor.start()
+# Cache.put("google.com", "Welcome to Google...")
+# Cache.get("google.com")
+# Cache.size()
+# Cache.put("amazon.com", nil)
 # boom! (ArgumentError) argument error :erlang.byte_size(nil)
+# Cache.size()
+# Cache.put("twitter.com", "cat photos")
+# Cache.get("twitter.com")
+# Cache.size()
