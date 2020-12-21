2c19e69f924a45d97718835fa6f2b3b0807fe2c7
Cristina Silva <crissilva.eng@gmail.com>
2020-12-21 05:21:26 +0000

    bump to 0.0.3-dev:wq

--- src/playground.exs
+++ src/playground.exs
@@ -1,5 +1,11 @@
-double = fn(x) -> x * 2 end
+double = fn x -> x * 2 end
 IO.inspect(double.(3))
 
-twice = fn(fun) -> fn(x) -> fun.(fun.(x)) end end
+twice = fn fun -> fn x -> fun.(fun.(x)) end end
 IO.inspect(twice.(double).(3))
+
+map = Map.new()
+map = Map.put(map, :a, "value for a")
+map = Map.put(map, :b, "value for b")
+
+IO.inspect(map)
